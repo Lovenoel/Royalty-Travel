003079f05dfc23a63621d9d686ac0ad1a255a881
lovenoel <kinglovenoel@gmail.com>
2024-09-25 07:22:50 +0300

    Code Update

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -34,7 +34,7 @@ def create_app():
     load_dotenv(os.path.join(basedir, '..', '.env'))
 
     # Set configuration from environment variables
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     
