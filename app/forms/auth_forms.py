fd7d033698fd43fe2f27b7ecffc4213b7952397b
lovenoel <kinglovenoel@gmail.com>
2024-10-07 18:25:28 +0300

    Corrected an import statement in auth_forms

--- app/forms/auth_forms.py
+++ app/forms/auth_forms.py
@@ -4,7 +4,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from models import User
+from models.users import User
 
 class RegistrationForm(FlaskForm):
     """Form for user registration"""    
