ac8afe981abc8b38fa601fb268ec5989bc5dfbb0
lovenoel <kinglovenoel@gmail.com>
2024-10-13 07:59:26 +0300

    Corrected code

--- app/forms/bookingForm.py
+++ app/forms/bookingForm.py
@@ -9,7 +9,7 @@ from wtforms.validators import DataRequired
 class BookingForm(FlaskForm):
     """Booking Form"""
     username  = StringField('Username', validators=[DataRequired('Username is required')])
-    passeneger_name = StringField('Passenger Name', validators=[DataRequired()])
+    passenger_name = StringField('Passenger Name', validators=[DataRequired()])
     departure_place = StringField('Departuure Place', validators=[DataRequired()])
     destination = StringField('Destianation', validators=[DataRequired()])
     departure_data_time = DateTimeField('Date & Time', format='%m/%d/%Y %H:%M', validators=[DataRequired()])
